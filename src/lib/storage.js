819e58870f0c30edea943a1038bd7989b33b2452
Joshua Fredrickson <uafredrickson@gmail.com>
2018-04-26 21:52:44 -0700

    adding refactor

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -2,32 +2,52 @@
 
 const logger = require('./logger');
 const storage = module.exports = {};
-const memory = {};
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'), { suffix: 'Prom' });
+
 
 storage.create = function create(schema, item) {
-  return new Promise((resolve, reject) => {
-    if (!schema) return reject(new Error('Connot create a new item, schema required'));
-    if (!item) return reject(new Error('Connot create a new item, item required'));
 
-    if (!memory[schema]) memory[schema] = {};
+  if (!schema) return reject(new Error('Connot create a new item, schema required'));
+  if (!item) return reject(new Error('Connot create a new item, item required'));
+  const json = JSON.stringify(item);
+  return fs.writeFileProm(`${__dirname}/../data/${schema}/${item.id}.json`, json)
+    .then(() => {
+      logger.log(logger.INFO, 'STORAGE: Created a new resource');
+      return item;
+    })
+    .catch(err => Promise.reject(err));
 
-    memory[schema][item.id] = item;
-    return resolve(item);
-  });
+  // if (!memory[schema]) memory[schema] = {};
+  //
+  // memory[schema][item.id] = item;
+  // return resolve(item);
+  // });
 };
 
 storage.fetchOne = function fetchOne(schema, id) {
-  return new Promise((resolve, reject) => {
     if (!schema) return reject(new Error('expected schema name'));
-    if (!id) return reject(new Error('expected id'));
-    if (!memory[schema]) return reject(new Error('schema not found'));
-    const item = memory[schema][id];
-    if (!item) {
-      return reject(new Error('item not found'));
-    }
-    return resolve(item);
-  });
-};
+    if (!id) return Promise.reject(new Error('expected id'));
+
+
+
+    return fs.readFileProm(`${__dirname}/../data${schema}/${id}.json`)
+        .then((data) => {
+          try {
+            const item = JSON.parse(data.toString());
+            return item;
+          } catch (err) {
+            return Promise.reject(err);
+          }
+        })
+        .catch((err) => {
+          logger.log(logger.ERROR, JSON.stringify(err));
+        });
+
+//
+//     return resolve(item);
+//   });
+// };
 
 storage.fetchAll = function fetchAll(schema) {
   return new Promise((resolve, reject) => {
